bc11193cada4b4d5872dd83e52881f0a0f97fecb
Bruno Serrate <brunostj@hotmail.com>
2020-04-08 17:17:51 -0300

    Carga inicial

---
+++ frontend/src/layouts/Website.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header class="bg-primary text-white" height-hint="98">
+      <q-toolbar class="q-pa-sm">
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+          </q-avatar>
+          Portfólio
+        </q-toolbar-title>
+        <q-tabs align="right">
+          <q-route-tab to="/" label="Home" />
+          <q-route-tab to="/sistemas" label="Sistemas" />
+        </q-tabs>
+      </q-toolbar>
+
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+};
+</script>
+
+<style>
+</style>
